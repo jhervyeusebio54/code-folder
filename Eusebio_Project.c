190b1886e768b45e969b3948bdd9e21f1af8e620
jhervy eusebio <jhervyeusebio54@gmail.com>
2026-05-17 15:52:41 +0800

    initial commit

--- Eusebio_Project.c
+++ Eusebio_Project.c
@@ -1,7 +1,4 @@
 /*
-Eusebio, John Jhervy G.
-BSIT-1b
-
 A simple Arithmetic Practice Program with the following features:
 1. Log/Sign in feature - Log in if the player has an existing account.
 Sign in if first time to play. (username, password)
@@ -16,11 +13,20 @@ Video recording of the Demo/Presentation of the program explaining the the featu
 */
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
+#include<string.h>
+#include<time.h>
 #define MAX 5
 
+char username[MAX][50];
+char password[MAX][20];
+int overall[MAX];
+int addscore[MAX];
+int subscore[MAX];
+int mulscore[MAX];
+int divscore[MAX];
+int total = 0;
+int current = -1;
 
-char name[MAX][50];
 int menu();
 int addition();
 int subtraction();
@@ -29,85 +35,235 @@ int division();
 void login();
 void display();
 void save();
+void load();
 void account();
 
-
 int main(){
-login();
-switch(menu()){
-case 1: system("cls");
+    int choice;
+    srand(time(NULL));
+    load();
+    login();
 
+    do{
+        choice = menu();
+        switch(choice){
+        case 1:
+            addition();
+            break;
+        case 2:
+            subtraction();
+            break;
+        case 3:
+            multiplication();
+            break;
+        case 4:
+            division();
+            break;
+        case 5:
+            save();
+            printf("Saved\n");
+            break;
+        default:
+            printf("Invalid\n");
 
-    break;
-case 2: system("cls");
-
-
-    break;
-case 3: system("cls");
-
-
-    break;
-case 4: system("cls");
-
-
-    break;
-case 5: system("cls");
-
-
-    break;
-
-
-
-
-
-
+        }
+        system("pause");
+        system("cls");
+    }while(choice!=5);
+    return 0;
 }
 
-
-
-
-return 0;
-}
 int menu(){
- int op;
-    system("cls");
-    printf("\nMenu\n");
-    printf("1. Addition\n");
+    int op;
+    printf("\n1. Addition\n");
     printf("2. Subtraction\n");
     printf("3. Multiplication\n");
     printf("4. Division\n");
     printf("5. Save and Exit\n");
-    printf("\nSelect(1-5): ");scanf("%d",&op);
+    printf("Select: ");
+    scanf("%d",&op);
     return op;
 }
+
 int addition(){
+    int a,b,ans,user;
+    a=rand()%50;
+    b=rand()%50;
+    ans=a+b;
+    printf("\n%d + %d = ",a,b);
+    scanf("%d",&user);
+    if(user==ans){
+        printf("Correct\n");
+        overall[current]++;
+        addscore[current]++;
 
+    }else{
+        printf("Wrong Answer: %d\n",ans);
 
+    }
+    return 0;
 }
+
 int subtraction(){
+    int a,b,ans,user;
+    a=rand()%100;
+    b=rand()%50;
+    ans=a-b;
+    printf("\n%d - %d = ",a,b);
+    scanf("%d",&user);
 
+    if(user==ans){
+        printf("Correct\n");
+        overall[current]++;
+        subscore[current]++;
+
+    }else{
+        printf("Wrong Answer: %d\n",ans);
+    }
+    return 0;
 }
+
 int multiplication(){
+    int a,b,ans,user;
+    a=rand()%12;
+    b=rand()%12;
+    ans=a*b;
+    printf("\n%d * %d = ",a,b);
+    scanf("%d",&user);
 
+    if(user==ans){
+        printf("Correct\n");
+        overall[current]++;
+        mulscore[current]++;
+
+    }else{
+        printf("Wrong Answer: %d\n",ans);
+    }
+    return 0;
 }
+
 int division(){
+    int a,b,ans,user;
+    b=(rand()%10)+1;
+    ans=(rand()%10)+1;
+    a=b*ans;
 
+    printf("\n%d / %d = ",a,b);
+    scanf("%d",&user);
+    if(user==ans){
+        printf("Correct\n");
+        overall[current]++;
+        divscore[current]++;
+    }else{
+        printf("Wrong Answer: %d\n",ans);
+    }
+    return 0;
 }
+
 void login(){
-char acc[50];
-char pass[10];
-printf("Enter Username:");
-scanf("%s",acc);
-printf("Enter Password:");
-scanf("%s", pass);
+    int choice;
+    int found=0;
+    char acc[50];
+    char pass[20];
+    printf("1. Login\n");
+    printf("2. Sign Up\n");
+    printf("Select: ");
+    scanf("%d",&choice);
+    if(choice==2){
+        account();
+        return;
+    }
+    printf("Enter Username: ");
+    scanf("%s",acc);
+    printf("Enter Password: ");
+    scanf("%s",pass);
+    for(int i=0;i<total;i++){
+        if(strcmp(acc,username[i])==0 &&
+           strcmp(pass,password[i])==0){
+            current=i;
+            found=1;
+        }
+    }
+
+    if(found){
+        printf("Login Success\n");
+        display();
+    }else{
+        printf("Account Not Found\n");
+        account();
+    }
 
 }
+
 void display(){
+    printf("\nLeaderboard\n");
+    for(int i=0;i<total;i++){
+        printf("\n%s\n",username[i]);
+        printf("Overall: %d\n",overall[i]);
+        printf("Addition: %d\n",addscore[i]);
+        printf("Subtraction: %d\n",subscore[i]);
+        printf("Multiplication: %d\n",mulscore[i]);
+        printf("Division: %d\n",divscore[i]);
+
+    }
 
 }
 void save(){
+    FILE *fp;
+    fp=fopen("records.txt","w");
+    if(fp==NULL){
+        return;
+    }
+    fprintf(fp,"%d\n",total);
+    for(int i=0;i<total;i++){
+        fprintf(fp,"%s %s %d %d %d %d %d\n",
+                username[i],
+                password[i],
+                overall[i],
+                addscore[i],
+                subscore[i],
+                mulscore[i],
+                divscore[i]);
+
+    }
+    fclose(fp);
+}
+
+void load(){
+    FILE *fp;
+    fp=fopen("records.txt","r");
+    if(fp==NULL){
+        return;
+
+    }
+
+    fscanf(fp,"%d",&total);
+    for(int i=0;i<total;i++){
+        fscanf(fp,"%s %s %d %d %d %d %d",
+               username[i],
+               password[i],
+               &overall[i],
+               &addscore[i],
+               &subscore[i],
+               &mulscore[i],
+               &divscore[i]);
+    }
+    fclose(fp);
 
 }
+
 void account(){
+    printf("Enter Username: ");
+    scanf("%s",username[total]);
+    printf("Enter Password: ");
+    scanf("%s",password[total]);
+    overall[total]=0;
+    addscore[total]=0;
+    subscore[total]=0;
+    mulscore[total]=0;
+    divscore[total]=0;
+    current=total;
+    total++;  
+    printf("Account Created\n");
 
 }
